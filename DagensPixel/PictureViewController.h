bfd8887fe7e8f96794dacb285e499ad12860a9f9
Viktor Söderman <viktor.soderman@hotmail.se>
2013-03-14 13:25:42 +0100

    back to Emily

---
+++ DagensPixel/PictureViewController.h
@@ -0,0 +1,18 @@
+//
+//  PictureViewController.h
+//  DagensPixel
+//
+//  Created by Viktor Söderman on 3/14/13.
+//  Copyright (c) 2013 LightStyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PictureViewController : UIViewController
+{
+    
+    IBOutlet UIScrollView *scroller;
+    
+}
+
+@end
---
+++ DagensPixel/PictureViewController.m
@@ -0,0 +1,46 @@
+//
+//  PictureViewController.m
+//  DagensPixel
+//
+//  Created by Viktor Söderman on 3/14/13.
+//  Copyright (c) 2013 LightStyle. All rights reserved.
+//
+
+#import "PictureViewController.h"
+
+@interface PictureViewController ()
+
+@end
+
+@implementation PictureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [scroller setScrollEnabled:YES];
+    [scroller setContentSize:CGSizeMake(320, 1000)];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
